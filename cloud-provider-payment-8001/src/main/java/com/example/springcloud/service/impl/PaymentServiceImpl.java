f94ce200b7febae90ca23e68f89c0668500bbfaa
xiaolh <acexlh@live.com>
2021-07-21 15:01:32 +0800

    增加公共模块

--- cloud-provider-payment-8001/src/main/java/com/example/springcloud/service/impl/PaymentServiceImpl.java
+++ cloud-provider-payment-8001/src/main/java/com/example/springcloud/service/impl/PaymentServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.springcloud.service.impl;
 import com.example.springcloud.dao.PaymentDao;
 import com.example.springcloud.entity.Payment;
 import com.example.springcloud.service.PaymentService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
